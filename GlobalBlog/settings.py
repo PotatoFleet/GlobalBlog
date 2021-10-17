f117c6257132197f72755c851f9bfddc56c5d6a9
PotatoFleet <supsrasri@gmail.com>
2021-10-17 22:09:14 +0530

    updated settings.py

--- GlobalBlog/settings.py
+++ GlobalBlog/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASE['default'].update(db_from_env) 
+DATABASES['default'].update(db_from_env) 
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
